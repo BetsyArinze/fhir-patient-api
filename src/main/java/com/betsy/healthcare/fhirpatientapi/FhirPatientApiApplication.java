6264d0190c40a5733854f6905cabcb3d7e6f0892
BetsyArinze <betarinze@gmail.com>
2026-07-01 13:42:32 +0100

    Initial project setup

---
+++ src/main/java/com/betsy/healthcare/fhirpatientapi/FhirPatientApiApplication.java
@@ -0,0 +1,13 @@
+package com.betsy.healthcare.fhirpatientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirPatientApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirPatientApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betsy/healthcare/fhirpatientapi/FhirPatientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betsy.healthcare.fhirpatientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirPatientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
